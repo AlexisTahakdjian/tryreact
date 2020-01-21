5d59a8eb181de73c3c98688a5e7fb16590b26e1a
Alexis <alexis.tahakdjian@gmail.com>
2020-01-21 12:43:39 +0100

    Exercice changement de bgColor quand on click sur le boutton

--- src/index.js
+++ src/index.js
@@ -1,19 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom'; 
 import './assets/style/index.css';
-import img from './assets/img/unsplash.jpg'
 import Header from './Header'
 import Footer from './Footer'
 import * as serviceWorker from './serviceWorker';
 
 
 class Main extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			bgColor: ""
+		};
+	}
+
+	buttonClick = () => {
+		let randomColor = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
+		this.setState({
+			bgColor: randomColor
+		});
+	}
+
 	render() {
 		return ( 
 			<div>
-				<Header / >
-				<div className="main">
-					<img className="img" src={img} alt="photo" />
+				<Header />
+				<div className="main" style={{ backgroundColor: this.state.bgColor }}>
+					<button
+						className='button'
+						onClick={this.buttonClick}
+					>
+						Changez la couleur</button>
 				</div> 
 				<Footer / >
 			</div>
@@ -29,4 +46,4 @@ ReactDOM.render( <Main/> ,
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();
